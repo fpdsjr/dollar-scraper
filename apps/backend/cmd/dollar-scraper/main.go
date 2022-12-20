14559fc9e2bdd3c09b8607f18fa14689ff487e8f
Flávio Junior <flavio.junior@tvnsports.com.br>
2022-12-20 04:17:05 -0300

    feat: add goworkspace

---
+++ apps/backend/cmd/dollar-scraper/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fpdsjr/dolar-scraper/scraper"
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWSDollar(ws *websocket.Conn) {
+	s.conns[ws] = true
+
+	for {
+		scraper.ScraperDollar()
+		websocket.Message.Send(ws, scraper.DollarPrice)
+	}
+}
+
+func main() {
+	s := NewServer()
+
+	fmt.Println("Websocket server running on port 9090")
+	http.Handle("/ws", websocket.Handler(s.handleWSDollar))
+	http.ListenAndServe(":8080", nil)
+}
